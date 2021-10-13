432e23f5dce741ef3d1dfb194cdcee5484a82666
Marc <bolverblitz@ebg.pw>
2021-10-13 05:29:58 +0200

    Added JS to add users on win to DB and load users from Db for the scoreboard
    I´m new to next.js thats why "LoadScore" button is there.

---
+++ pages/api/labyrinth/getUsers.js
@@ -0,0 +1,35 @@
+const pg = require('pg');
+require('dotenv').config()
+
+const pool = new pg.Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+})
+
+/**
+ * Promise to return all users
+ * @returns {Object}
+ */
+ let GetAllGuests = function() {
+  return new Promise(function(resolve, reject) {
+    pool.query(`SELECT username FROM scoreuser`, (err, result) => {
+      if (err) {reject(err)}
+      resolve(result.rows);
+    });
+  });
+}
+
+pool.query(`CREATE TABLE IF NOT EXISTS scoreuser (
+    username text PRIMARY KEY,
+    time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP)`, (err, result) => {
+    if (err) {console.log(err)}
+});
+
+export default function handler(req, res) {
+  GetAllGuests().then(function(AllUser_Response) {
+    res.status(200).json( AllUser_Response )
+  });
+}
--- pages/labyrinth.js
+++ pages/labyrinth.js
@@ -7,10 +7,11 @@ function Labyrinth() {
             <title>Labyrinth</title>
         </Head>
         <div className={styles.intro}>
-        <h2>Labyrinth by artsykiky</h2>
-        <p>Reach the end from the beginning.</p>
-        <p>Click on the numbers as you pass them.</p>
-    </div>
+            <h2>Labyrinth by artsykiky</h2>
+            <p>Reach the end from the beginning.</p>
+            <p>Click on the numbers as you pass them.</p>
+            <p><input id="username" type="text" placeholder="Your Username" /></p>
+        </div>
 
         <ul className={styles.area}>
             <li className={styles.first}></li>
@@ -36,7 +37,7 @@ function Labyrinth() {
             <li className={styles.level4}></li>
             <li className={styles.level4}></li>
             <li className={styles.level4}></li>
-            <li className={styles.level4}></li>
+            <li className={styles.level4} onMouseOver={handleWin}></li>
             <li className={styles.level4}></li>
 
             <li className={styles.empty}></li>
@@ -47,7 +48,59 @@ function Labyrinth() {
                 <h1>Winning doesn´t always mean being first. Winning means you´re doing better than you´ve ever done before.</h1>
             </li>
         </ul>
-        <p>Winners:</p></div>
+        <p id="Winners"><button onClick={handleScore}>LoadScore</button></p>
+    </div>
+}
+
+const handleWin = async (e) =>  {
+    const getUrl = window.location;
+    const baseUrl = getUrl.protocol + "//" + getUrl.host + "/";
+    const Username = document.getElementById('username').value
+
+    if(Username.length >= 1){
+        const result = await fetch(`${baseUrl}/api/labyrinth/handleWin?Username=${Username}`);
+        if(result.status === 200){
+            console.log(`Added Username ${Username}`)
+            fetch(`${baseUrl}/api/labyrinth/getUsers`)
+            .then((res) => { 
+                return res.json() 
+            })
+            .then((jsonData) => {
+                let UserScoreList = []
+                jsonData.map( User => {
+                    UserScoreList.push(User.username)
+                })
+                document.getElementById('Winners').innerHTML = UserScoreList.join('<br>')
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+        }else if(result.status === 400){
+            alert("Username already exists")
+        }
+    }else{
+        alert("No username was given")
+    }
+}
+
+const handleScore = (e) =>  {
+    const getUrl = window.location;
+    const baseUrl = getUrl.protocol + "//" + getUrl.host + "/";
+
+    fetch(`${baseUrl}/api/labyrinth/getUsers`)
+    .then((res) => { 
+        return res.json() 
+      })
+      .then((jsonData) => {
+          let UserScoreList = []
+        jsonData.map( User => {
+            UserScoreList.push(User.username)
+        })
+        document.getElementById('Winners').innerHTML = UserScoreList.join('<br>')
+      })
+      .catch((err) => {
+        console.error(err);
+      });
 }
 
 export default Labyrinth
