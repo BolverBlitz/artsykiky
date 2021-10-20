b9d07181b226b7dbe62aae018337aedcfd08a699
Marc <bolverblitz@ebg.pw>
2021-10-20 18:21:56 +0200

    Added list to index

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,23 @@ import Link from "next/link"
 export default function Home() {
   return (
     <>
-    <Link href="/labyrinth">LABYRINTH</Link>
-    <Link href="/3Dcube">3Dcube</Link>
-    <Link href="/SquidGameRedlightGreenlight">Squid Game Redlight Greenlight</Link>
+    <ul>
+      <li>
+        <Link href="LABYRINTH">
+          <a>Labyrinth</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/3Dcube">
+          <a>3Dcube</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/SquidGameRedlightGreenlight">
+          <a>Squid Game Redlight Greenlight</a>
+        </Link>
+      </li>
+    </ul>
     </>
   )
 }
